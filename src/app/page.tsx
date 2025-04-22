55c2f618a43e2ce5e861a7ebf242108a39314d55
T14Xploit <khrystyna.tyvoniuk@gmail.com>
2025-04-22 15:33:08 +0200

    added categories menu for the landing page with search and buttton live

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,45 @@
+import { Button } from "@/components/ui/button";
+import { Search } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
+  //temp
+  const categories = [
+    'Sports', 'Politics', 'Technology', 'Finance', 'Education',
+    'Entertainment', 'Art', 'Culture', 'Local'
+  ]
   return (
- <>
- <h1 className="font-instrument">
-  Welcome to Homepage
- </h1>
-  </>
+ 
+<main className="flex flex-col justify-between py-8 px-6 bg-background text-foreground font-intrument">
+      <div className="flex items-center gap-4 mb-6 font-instrument">
+        <Button variant="destructive" >
+          LIVE 🔴
+        </Button>
+
+        <div className="ml-20 flex gap-4 overflow-x-auto">
+          {categories.map((category, index) => (
+            <Link
+              key={index}
+              href={`/${category.toLowerCase()}`}
+              className="px-4 py-2 rounded-lg hover:bg-muted hover:text-primary transition whitespace-nowrap"
+            >
+              {category}
+            </Link>
+          ))}
+        </div>
+        
+        <div className="ml-auto">
+          <Button variant="link" aria-label="Search" >
+            <Search />
+          </Button>
+        </div>
+      </div>
+
+      {/* Additional Content Below */}
+      <div className="text-center">
+        {/* You can add more content below as needed */}
+      </div>
+    </main>
+  
   );
 }
