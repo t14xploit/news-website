6b46e7bf0c99b8649281d23619352c43502b6bca
T14Xploit <khrystyna.tyvoniuk@gmail.com>
2025-04-22 13:11:52 +0200

    migration with prisma models + prisma global var

---
+++ prisma/migrations/20250422110724_categories_articles/migration.sql
@@ -0,0 +1,42 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Article" (
+    "id" TEXT NOT NULL,
+    "headline" TEXT NOT NULL,
+    "summary" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "image" TEXT,
+    "author" TEXT NOT NULL,
+    "views" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Article_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_ArticleToCategory" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_ArticleToCategory_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_title_key" ON "Category"("title");
+
+-- CreateIndex
+CREATE INDEX "_ArticleToCategory_B_index" ON "_ArticleToCategory"("B");
+
+-- AddForeignKey
+ALTER TABLE "_ArticleToCategory" ADD CONSTRAINT "_ArticleToCategory_A_fkey" FOREIGN KEY ("A") REFERENCES "Article"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ArticleToCategory" ADD CONSTRAINT "_ArticleToCategory_B_fkey" FOREIGN KEY ("B") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ src/lib/prisma.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from '@/generated/prisma';
+
+const prisma = global.__prisma||new PrismaClient()
+
+declare global{
+    var __prisma: PrismaClient |undefined;
+}
+
+if(process.env.NODE_ENV !== "production") global.__prisma = prisma;
+export{prisma};
