f30d842f63cd48fb7a90d6e6b40f1d007fc2a18b
T14Xploit <khrystyna.tyvoniuk@gmail.com>
2025-04-28 07:55:11 +0200

    design fixes

--- src/app/authors/[id]/page.tsx
+++ src/app/authors/[id]/page.tsx
@@ -11,7 +11,7 @@ interface AuthorPageProps {
 
 const AuthorPage: FC<AuthorPageProps> = async ({ params }) => {
   const author = await prisma.author.findUnique({
-    where: { id: params.id },
+    where: { id: await params.id },
     include: {
       articles: {
         orderBy: {
@@ -29,7 +29,7 @@ const AuthorPage: FC<AuthorPageProps> = async ({ params }) => {
   }
 
   return (
-    <div className="container space-y-6">
+    <div className="container space-y-6 mb-6">
         <div className="mt-6 flex items-center space-x-4">
           {/* Avatar */}
           <Avatar className="w-35 h-35">
@@ -50,7 +50,7 @@ const AuthorPage: FC<AuthorPageProps> = async ({ params }) => {
 
       {/* Articles Section */}
       <h2 className="text-lg font-semibold flex items-center">Author's Articles <ArrowBigRight className="w-6 h-6 text-foreground" /></h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {author.articles.map((article) => (
           <Card key={article.id} className="p-4 space-y-4 shadow-lg hover:shadow-xl transition-all">
             {/* Article Card */}
