5fed0ad732ce2679466201d4e4a6165d8c0d8099
T14Xploit <khrystyna.tyvoniuk@gmail.com>
2025-04-30 13:22:44 +0200

    second-api

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/spotprices', 
+        destination: 'https://spotprices.lexlink.se/espot', //  API URL
+      },
+    ];
+  },
 
   images: {
     remotePatterns: [
