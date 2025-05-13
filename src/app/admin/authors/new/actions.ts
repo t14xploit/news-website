a9861e905553cd820706d255bc0b741ec98a2756
T14Xploit <khrystyna.tyvoniuk@gmail.com>
2025-05-13 17:27:56 +0200

    create author

---
+++ src/app/admin/authors/new/actions.ts
@@ -0,0 +1,44 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+const authorSchema = z.object({
+  name: z.string().min(2, "Author name must be at least 2 characters"),
+  image: z.union([z.string().url(), z.literal("")]),
+});
+
+export async function createAuthor(prevState: unknown, formData: FormData) {
+  const obj = Object.fromEntries(formData.entries());
+  const result = authorSchema.safeParse(obj);
+
+  if (!result.success) {
+    return {
+      message: "Attempt to create author failed",
+      error: result.error.errors.map(e => e.message).join(", "),
+    };
+  }
+
+  const { name, image } = result.data;
+
+  const existingAuthor = await prisma.author.findFirst({
+    where: { name },
+  });
+
+  if (existingAuthor) {
+    return {
+      message: "Author already exists!",
+      error: "Error: Author with that name already exists.",
+    };
+  }
+
+  await prisma.author.create({
+    data: {
+      name,
+      picture: image, // match your model field
+    },
+  });
+
+  redirect("/admin/authors");
+}
---
+++ src/app/admin/authors/new/page.tsx
@@ -0,0 +1,11 @@
+import CreateAuthorForm from "./form";
+
+export default async function page() {
+ 
+  return (
+    <div>
+      <h1>Create Author</h1>
+      <CreateAuthorForm />
+    </div>
+  );
+}
